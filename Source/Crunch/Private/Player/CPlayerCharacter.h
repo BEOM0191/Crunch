74a29e4586f70ca10dfda7afe8dd19ba0e328222
jingtian <lxwk2art@live.com>
2024-07-09 08:30:18 -0500

    08 Add the Look Input Action

--- Source/Crunch/Private/Player/CPlayerCharacter.h
+++ Source/Crunch/Private/Player/CPlayerCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Character/CCharacter.h"
+#include "InputActionValue.h"
 #include "CPlayerCharacter.generated.h"
 
 /**
@@ -26,7 +27,12 @@ private:
 
 	UPROPERTY(EditDefaultsOnly, Category = "Input")
 	class UInputAction* JumpInputAction;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* LookInputAction;
 
 	UPROPERTY(EditDefaultsOnly, Category = "Input")
 	class UInputMappingContext* GameplayInputMappingContext;
+
+	void HandleLookInput(const FInputActionValue& InputActionValue);
 };
