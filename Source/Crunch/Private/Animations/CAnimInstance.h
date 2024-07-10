75d5c6c338b34a2d6ed6c340574b0acc9b6899f6
jingtian <lxwk2art@live.com>
2024-07-09 19:29:53 -0500

    14 Add Look Offset

--- Source/Crunch/Private/Animations/CAnimInstance.h
+++ Source/Crunch/Private/Animations/CAnimInstance.h
@@ -44,6 +44,12 @@ public:
 
 	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
 	FORCEINLINE bool GetIsOnGround() const { return !bIsJumping; }
+
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE float GetLookYawOffset() const { return LookRotOffset.Yaw; }
+
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE float GetLookPitchOffset() const { return LookRotOffset.Pitch; }
 private:
 	UPROPERTY()
 	class ACharacter* OwnerCharacter;
@@ -59,4 +65,5 @@ private:
 	float YawSpeedSmoothLerpSpeed = 1.f;
 
 	FRotator BodyPrevRot;
+	FRotator LookRotOffset;
 };
